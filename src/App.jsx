fb4bc0618ad6d255f1efaf20d6f8be64e840c79e
Tamal Das <gyansujan69@gmail.com>
2022-02-20 22:40:04 +0530

    feat: Backend setup, code refactor

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/Home'
+
+
+const App = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+//* This is the Home Component , all the other components will be called in here
+
+import React from 'react'
+import LandingPage from './LandingPage'
+
+const Home = () => {
+    return (
+        <>
+            <div>Home</div>
+            <LandingPage />
+
+        </>
+    )
+}
+
+export default Home
