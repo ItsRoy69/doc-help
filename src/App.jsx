f00ba80dd76f367b391bad0ef310347ea8438732
Tamal Das <gyansujan69@gmail.com>
2022-02-22 13:39:17 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 
 
 const App = () => {
     return (
         <>
-            <Home />
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </Router>
+
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 //* This is the Home Component , all the other components will be called in here
 
 import React from 'react'
+import Navbar from '../components/Navbar'
 import LandingPage from './LandingPage'
 
 const Home = () => {
     return (
         <>
-            <div>Home</div>
+            <Navbar />
             <LandingPage />
 
         </>
