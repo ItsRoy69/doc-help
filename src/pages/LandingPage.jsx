e9430b67d6e8126d94dc273307e8b37941579758
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-03-12 21:13:42 +0530

    updated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,25 +4,45 @@ import '../styles/LandingPage.css';
 
 const LandingPage = () => {
   return (
-      <div className="banner" id="banner">
-        <div className="banner-image">
-          <img src={bannermed} alt="bannerimg" />
-        </div>
-        <div className="banner-content">
-          <h1 className="banner__text">Some Tagline here</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-          <div className="banner__input">
-            <div className="banner__up">
-              <button type="button">Sign Up</button>
-            </div>
-            <div className="banner__in">
-              <button type="button">Sign In</button>
+      <>
+        <div className="banner" id="banner">
+          <div className="banner-image">
+            <img src={bannermed} alt="bannerimg" />
+          </div>
+          <div className="banner-content">
+            <h1 className="banner__text">Some Tagline here</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <div className="banner__input">
+              <div className="banner__up">
+                <button type="button">Sign Up</button>
+              </div>
+              <div className="banner__in">
+                <button type="button">Sign In</button>
+              </div>
             </div>
           </div>
         </div>
+        <div className="Services" id="Services">
+          <div className="service__heading">
+            <h1>Services</h1>
+          </div>
+          <div className="service__boxes">
+            <div className="service__box1">
+              <img src={bannermed} alt="bannerimg" />
+            </div>
+            <div className="service__box2">
+              <img src={bannermed} alt="bannerimg" />
+            </div>
+            <div className="service__box3">
+              <img src={bannermed} alt="bannerimg" />
+            </div>
+            <div className="service__box4">
+              <img src={bannermed} alt="bannerimg" />
+            </div>
+          </div>
+        </div>
+      </>
 
-
-      </div>
   )
 }
 
