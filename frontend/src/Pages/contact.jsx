16219f16509e6adbda01cc43e1cb6ff132cca49c
Tamal Das <gyansujan69@gmail.com>
2022-03-25 09:46:30 +0530

    feat: connected DB

--- frontend/src/Pages/contact.jsx
+++ frontend/src/Pages/contact.jsx
@@ -10,55 +10,55 @@ import './contact.css';
 const contact = () => {
   return (
     <>
-      <Navbar/>
-        <div className="about">
-          <h1>About Us</h1>
-          <div className="about__boxes">
-            <div className="about__box">
-              <div className="about__boximg">
-                <img src={pic2} alt="servicesimg" />
-              </div>
-              <h1>Tamal Das</h1>
-              <p>Backend Developer</p>
+      <Navbar />
+      <div className="about">
+        <h1>About Us</h1>
+        <div className="about__boxes">
+          <div className="about__box">
+            <div className="about__boximg">
+              <img src={pic2} alt="servicesimg" />
             </div>
-            <div className="about__box">
-              <div className="about__boximg">
-                <img src={pic1} alt="aboutsimg" />
-              </div>
-              <h1>Jyotirmoy Roy</h1>
-              <p>Team Lead & Developer</p>
-            </div>
-            <div className="about__box">
-              <div className="about__boximg">
-                <img src={pic3} alt="aboutsimg" />
-              </div>
-              <h1>Parna Chowdhury</h1>
-              <p>Designer & Technical Writer</p>
-            </div>
+            <h1>Tamal Das</h1>
+            <p>Backend Developer</p>
           </div>
-
-          <div className="contact" id="Contact">
-                <div className="contact__box">
-                  <div className="contact__meta">
-                    <h1>Contact Us</h1>
-                    <p>You can contact us via mail</p>
-                  </div>
-                  <div className="boxes">
-                    <div className="input__box">
-                       <input type="text" className="contact name" placeholder="Your name *" />
-                       <input type="text" className="contact email" placeholder="Your Email *" />
-                       <input type="text" className="contact subject" placeholder="Write a Subject" />
-                       <textarea name="message" id="message" placeholder="Write Your message"></textarea>
-                       <a href="mailto:jyotirmoyroy649@gmail.com"><button className="btn contact pointer" type="submit">Submit</button></a>
-                    </div>
-                    <div className="image">
-                        <img src={illustration1} alt="" className="contact__img" />
-                    </div>
-                  </div>
-                </div>
+          <div className="about__box">
+            <div className="about__boximg">
+              <img src={pic1} alt="aboutsimg" />
+            </div>
+            <h1>Jyotirmoy Roy</h1>
+            <p>Team Lead & Developer</p>
+          </div>
+          <div className="about__box">
+            <div className="about__boximg">
+              <img src={pic3} alt="aboutsimg" />
+            </div>
+            <h1>Parna Chowdhury</h1>
+            <p>Designer & Technical Writer</p>
           </div>
         </div>
-      <Footer/>
+
+        <div className="contact" id="Contact">
+          <div className="contact__box">
+            <div className="contact__meta">
+              <h1>Contact Us</h1>
+              <p>You can contact us via mail</p>
+            </div>
+            <div className="boxes">
+              <div className="input__box">
+                <input type="text" className="contact name" placeholder="Your name *" />
+                <input type="text" className="contact email" placeholder="Your Email *" />
+                <input type="text" className="contact subject" placeholder="Write a Subject" />
+                <textarea name="message" id="message" placeholder="Write Your message"></textarea>
+                <a href="mailto:jyotirmoyroy649@gmail.com"><button className="btn contact pointer" type="submit">Submit</button></a>
+              </div>
+              <div className="image">
+                <img src={illustration1} alt="" className="contact__img" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
